9d84a9d4a594cc13b153825470ac2d8653123621
Outloudvi <olvi.git@mailnull.com>
2018-06-24 09:23:45 +0800

    Add global.misaka

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+global.misaka = {};
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var quitRouter = require('./routes/quit');
+var loadRouter = require('./routes/load');
 
 var app = express();
 
@@ -21,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/quit', quitRouter);
+app.use('/load',loadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
