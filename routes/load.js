d83d5030649aebdbd5739a286908db135c4ff43f
Outloudvi <olvi.git@mailnull.com>
2018-06-24 09:28:18 +0800

    Add robust to load.js

--- routes/load.js
+++ routes/load.js
@@ -3,23 +3,36 @@ var router = express.Router();
 var fs = require('fs');
 
 router.get('/', function (req, res, next) {
+  let noerr = true;
   try {
     let fsconf = fs.readFileSync(process.cwd() + "/config.json", "utf8");
+    if (fsconf == "") {
+      fsconf = "{}";
+      console.info("Config initalized.");
+    }
     global.misaka.config = JSON.parse(fsconf);
   } catch (err) {
     console.log(err);
+    noerr = false;
     res.sendStatus(500);
   }
 
   try {
     let fsconf = fs.readFileSync(process.cwd() + "/data.json", "utf8");
+    if (fsconf == "") {
+      fsconf = "{}";
+      console.info("Data initalized.");
+    }
     global.misaka.data = JSON.parse(fsconf);
   } catch (err) {
     console.log(err);
+    noerr = false;
     res.sendStatus(500);
   }
-
-  res.sendStatus(200);
+  if (noerr) {
+    console.info("Config & data loaded.");
+    res.sendStatus(200);
+  }
 });
 
 module.exports = router;
